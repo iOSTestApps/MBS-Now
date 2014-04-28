84ec93e1a22dc525fff273d98ff6a1e527276752
Graham Dyer <gdyer2@illinois.edu>
2014-04-28 19:16:04 -0400

    Full meta/index.php file
    Take a look at campus.mbs.net/mbsnow/home/meta for a live copy.

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../docs-assets/ico/favicon.ico">
+        <title>MBS Now, meta index</title>
+        <link href="../dist/css/bootstrap.css" rel="stylesheet">
+        <link href="../jumbotron-narrow.css" rel="stylesheet">
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="header">
+                <ul class="nav nav-pills pull-right">
+                    <li><a href="../index.php">Home</a>
+
+                    </li>
+                    <li><a href="../forms">Forms</a>
+
+                    </li>
+                    <li><a href="../clubs.php">Clubs</a>
+
+                    </li>
+                    <li><a href="../code">Code</a>
+
+                    </li>
+                    <li class="active"><a href="#">More</a>
+
+                    </li>
+                </ul>
+                 <h3 class="text-muted">MBS Now</h3>
+
+            </div>
+             <h2>Meta</h2>
+
+            <ul class="pagination">
+                <li><a href="privacy.php">Privacy information</a>
+
+                </li>
+                <li><a href="../report.html">Report a bug</a>
+
+                </li>
+                <li><a href="https://www.facebook.com/mobeardlabs">Like us on Facebook</a>
+
+                </li>
+                <li><a href="../code/screenshots/">Screenshots</a>
+
+                </li>
+            </ul>
+            <button type="button" class="btn btn-sm btn-success" onclick="location.href='http://campus.mbs.net/mbs/widget/ScheduleGraphic.wdgt.zip';" ">Download OS X Dashboard schedule widget</button><p class="text-muted " style="display:inline ">   Direct download</p>
+
+  <h3><a id="versions ">Version history</a></h3>
+	<table class="table table-hover ">
+	<tr>
+		<th>Number</th>
+		<th>Description</th>
+		<th>Available since</th>
+		<th>Supported OS's</th>
+	</tr>
+	<tr>
+		<td>3.5.1</td>
+		<td>Support for iOS 7.1, bug fixes.</td>
+		<td>?</td>
+		<td>iOS 7.1 and earlier</td>
+	</tr>
+	<tr>
+		<td>3.4.2</td>
+		<td>New look for tables, RSVP and bug fixes.</td>
+		<td>March 22, 2014</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+	<tr>
+		<td>3.3.8</td>
+		<td>Automatic RSVPs, meeting data have been restructured, all connections are now asynchronous (preventing delays), "What 's New" for iPhone, in-app meeting modification notifications, unified forms, completely autonomous form uploads, lunch distinctions update faster, bug fixes.</td>
+		<td>February 6, 2014</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+	<tr>
+		<td>3.2.5</td>
+		<td>Custom alert times, improvements for viewing and creating meetings, explore campus improvements, connection and bug fixes.</td>
+		<td>December 4, 2013</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>3.1.1</td>
+		<td>Small changes with clubs, resolved connectivity problems, semi-accommodation for the new mbs.net.</td>
+		<td>November 20, 2013</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>3.0.9</td>
+		<td>Meetings! Create, view, and share meetings all through the app; HTML formatted outgoing emails; bug fixes.</td>
+		<td>November 7, 2013</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>2.4.6</td>
+		<td>Automatic form uploads, iOS 7 support, explore campus, instant alerts, bug reporter, design improvements, early/late lunch listings, bug fixes.</td>
+		<td>October 29, 2013</td>
+		<td>iOS 7 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>2.3.1</td>
+		<td>Notification fixes, lunch menu fixes, dynamic temporary form titles.</td>
+		<td>September 8, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>2.2.9</td>
+		<td>Automatic data uploads, unified settings manager, contact MBS improvements, design facelift, bug fixes.</td>
+		<td>August 30, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>2.1.9</td>
+		<td>Research database credentials, notifications added for A/B weeks and general announcements, massive package reduction, stability & bug fixes.</td>
+		<td>August 12, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>2.0.5</td>
+		<td>Data exporting, notifications for dress-up days, unified listings manager, Middle School homework & special schedule improvements.</td>
+		<td>July 31, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>1.3</td>
+		<td>773.7% reduction in package size, higher quality images, swipe gestures to change backgrounds, design improvements.</td>
+		<td>July 19, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>1.2</td>
+		<td>Massive design improvements, photo browser, countdown, form search, bug fixes.</td>
+		<td>June 27, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>1.1</td>
+		<td>Special schedules, lunch menu improvements, export forms through email, temporary forms, bug fixes.</td>
+		<td>July 19, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+
+	<tr>
+		<td>1.0</td>
+		<td>Bare essentials :/</td>
+		<td>August 29, 2013</td>
+		<td>iOS 6 and earlier</td>
+	</tr>
+   </table>
+
+<h3><a id="credits">Credits</a></h3>
+	<table class="table">
+	<tr>
+		<th>Name</th>
+		<th>Contribution</th>
+	</tr>
+	<tr>
+		<td><a href="mailto:lfagan@mbs.net">Lucas Fagan</a></td>
+		<td><span class="label label-default">Future</span> Programming, design, and content of MBS Now. HTML, PHP, and content of this site.</td>
+	</tr>
+	<tr>
+		<td><a href="http://gdyer.de/g">Graham Dyer</a></td>
+		<td><span class="label label-default">Original</span> Programming, design, and content of MBS Now. HTML, PHP, and content of this site.</td>
+	</tr>
+	<tr>
+		<td>Mrs. Barbara Napholtz</td>
+		<td>PHP, schedule widget and database</td>
+	</tr>
+	<tr>
+	<td>Lena Rajan</td>
+		<td>App icon and launch screen</td>
+	</tr>
+	<tr>
+		<td>S. Vermette</td>
+		<td><a href="https://github.com/samvermette/SVProgressHUD">SVProgressHUD</a>, an open source Cocoa class used by MBS Now.</td>
+	</tr>
+</table>
+
+<h3><a id="about">About us</a></h3>
+	MBS Now was made with the idea to make students', parents ', and faculty members' lives easier. Before MBS Now, there was no medium from which those associated with the school could access crucial information. Hopefully you think this has changed now! We 're facilitating Morristown-Beard's mission of having technology in the classroom. As every student is required to have an iPad, an app 's impact can be felt immediately and on a large scale.
+	<br><br>
+	We work differently than most high-school and college applications. Rather than being controlled by a company that runs a myriad of apps that have all been formatted to the same template, MBS Now is much more dynamic. We listen to your suggestions and add functionality accordingly.
+	<br><br>
+	Furthermore, MBS Now will always be maintained by students; as soon as a student-developer moves on, the app and its responsibilities will be passed down to underclassmen.
+	<br><br>
+	<a href="mailto:team@gdyer.de">Let us know</a> if you have any questions, suggestions, or feedback.
+	We hope you enjoy the app!</p>
+
+	<br>
+	<div class="footer">
+        <?php $last_modified = filemtime("index.php"); print("Last modified "); print(date("m/j/y", $last_modified)); ?>
+    </div>
+  </body>
+</html>
