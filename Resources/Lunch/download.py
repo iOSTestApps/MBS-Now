09108866beca8b52adc9c4e8217708285f58677a
gdyer <9fermat@gmail.com>
2014-05-03 22:43:23 -0400

    add menu downloder python script

---
+++ Resources/Lunch/download.py
@@ -0,0 +1,38 @@
+# works with Python 3 and above
+# run this on a Saturday or Sunday
+import urllib.request, urllib.parse, urllib.error, time, datetime
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+last = 0
+
+for i in range(len(days) + 1):
+    q = (datetime.datetime.today().weekday() + i) % len(days)
+    if (days[q] in weekdays):
+        unformatted = datetime.date.today() + datetime.timedelta(days=i)
+        last = unformatted
+        formatted = unformatted.strftime('%m/%d/%y')
+        url = 'http://myschooldining.com/mbs/?cmd=pdfmenuday&currDT=' + formatted
+        print(url)
+        f = urllib.request.urlopen(url)
+        data = f.read()
+        with open("/Users/gdyer/Dropbox/MBS-Now/Resources/Lunch/" + days[q] + ".pdf", "wb") as code:
+            code.write(data)
+
+# now do the monthly menu
+if (last.month != datetime.datetime.today().month):
+    print("Conflicting months detected. Storing next month as 'Month2.pdf'.")
+    url = 'http://myschooldining.com/mbs/createPDFMenuMonthAct.cfm?currDT=' + last.strftime('%m/%d/%y')
+    print(url)
+    f = urllib.request.urlopen(url)
+    data = f.read()
+    with open("/Users/gdyer/Dropbox/MBS-Now/Resources/Lunch/" + "Month2.pdf", "wb") as code:
+        code.write(data)
+
+url = 'http://myschooldining.com/mbs/createPDFMenuMonthAct.cfm?currDT=' + datetime.datetime.today().strftime('%m/%d/%y')
+print(url)
+f = urllib.request.urlopen(url)
+data = f.read()
+with open("/Users/gdyer/Dropbox/MBS-Now/Resources/Lunch/" + "Month.pdf", "wb") as code:
+    code.write(data)
