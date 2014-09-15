f8c16a3f4242f7a7d1f6ca2d65d8ce4fd01c7b9f
gdyer <9fermat@gmail.com>
2014-09-14 19:44:23 -0500

    see last message

--- Resources/pull.py
+++ Resources/pull.py
@@ -1,8 +1,5 @@
 import os
-from os.path import expanduser
-home = expanduser("~")
 
-to_cd = home + "/Dropbox/MBS-Now/"
-print(to_cd)
+to_cd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(to_cd)
-os.system("git pull https://github.com/mbsdev/MBS-Now.git")
+os.system("git pull https://github.com/mbsdev/MBS-Now")
--- Resources/push.py
+++ Resources/push.py
@@ -1,21 +1,18 @@
 import os
-from os.path import expanduser
-home = expanduser("~")
-to_cd = home + "/Dropbox/MBS-Now/"
+
+to_cd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(to_cd)
 
 auto_pull = input('Auto-pull now? (y/anything): ')
-if auto_pull is 'y':
-	os.system("git pull https://github.com/mbsdev/MBS-Now.git")
-	
+if auto_pull == 'y':
+	os.system("git pull https://github.com/mbsdev/MBS-Now")
 auto_commit = input('Commit message (type "q" to quit): ')
-if auto_commit is 'q':
-	raise SystemExit
+if auto_commit == 'q':
+	exit(0)
 
-os.system("git add -A Resources/")
-os.system("git add -A MBS_Now/")
-os.system("git add README.md")
+os.system("git rm -r --cached .")
+os.system("git add .")
 os.system("git commit -m '" + auto_commit.strip("'") + "'")
 os.system("git remote rm origin")
-os.system("git remote add origin https://github.com/mbsdev/MBS-Now.git")
+os.system("git remote add origin https://github.com/mbsdev/MBS-Now")
 os.system("git push origin master")
